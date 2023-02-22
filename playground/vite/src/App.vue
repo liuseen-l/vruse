64f9c1498f1f1cbbe22411b74f43f63b848c2eb6
code-ManL <77389440@qq.com>
2023-02-22 15:45:51 +0800

    chore: packages

--- playground/vite/src/App.vue
+++ playground/vite/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { reactive } from "vue";
-import { usePick } from "@vruse/core/vue/vue";
+import { usePick } from "@vruse/core";
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const r = reactive<number[]>([]);
