ab7f346a13142558832c39d5b5391c55eb9ab33b
code-ManL <77389440@qq.com>
2023-02-23 22:33:46 +0800

    chore: test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,11 @@
-import { resolve } from 'path'
+// import { resolve } from 'path'
+import type { UserConfig } from 'vitest/config'
 import { defineConfig } from 'vitest/config'
 
+// ts-expect-error let me do it
 export default defineConfig({
   resolve: {
-    dedupe: [
-      'vue',
-      'vue-demi',
-      '@vue/runtime-core',
-    ],
+    dedupe: ['vue', 'vue-demi', '@vue/runtime-core'],
   },
   define: {
     __VUE_OPTIONS_API__: 'true',
@@ -16,13 +14,11 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: [resolve(__dirname, 'packages/.test/setup.ts')],
+    // setupFiles: [resolve(__dirname, 'packages/.test/setup.ts')],
     reporters: 'dot',
+    include: ['packages/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     deps: {
-      inline: [
-        '@vue/composition-api',
-        'vue-demi',
-      ],
+      inline: ['@vue/composition-api', 'vue-demi'],
     },
   },
-})
+}) as UserConfig
