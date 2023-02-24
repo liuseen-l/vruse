0526db0d4ea9fbe313bd78193d284d76c974ead5
code-ManL <77389440@qq.com>
2023-02-24 18:16:02 +0800

    chore: docs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@ import DefaultTheme from 'vitepress/theme'
 import 'uno.css'
 import './styles/vars.css'
 import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
+
 import Home from './components/Home.vue'
 
 export default {
