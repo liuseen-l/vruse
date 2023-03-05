1302f19c3a3387caae5c635e48739eba3df38dd4
code-ManL <773890440@qq.com>
2023-03-05 19:00:18 +0800

    fix: ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,13 +12,18 @@ const modeResolver: { [key: string]: () => any } = {
   }),
 }
 
+function isUnitTest(u: string) {
+  return u === 'r' || u === 'v'
+}
+
 export default defineConfig(({ mode }) => {
+  const testConfig = isUnitTest(mode) ? modeResolver[mode]() : []
   return {
     resolve: {
       dedupe: ['vue', 'vue-demi', '@vue/runtime-core'],
     },
     test: {
-      ...modeResolver[mode](),
+      ...testConfig,
       globals: true,
       environment: 'jsdom',
       setupFiles: [resolve(__dirname, './scripts/setupVitest.ts')],
