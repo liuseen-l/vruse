75c9a944ca626aec127838f37e8e6592bfc52f89
code-ManL <773890440@qq.com>
2023-03-06 13:29:49 +0800

    feat: publish

--- packages/vue/usePick/__tests__/index.spec.ts
+++ packages/vue/usePick/__tests__/index.spec.ts
@@ -22,33 +22,4 @@ describe('usePick', () => {
       'Please call it again after the execution is complete',
     ).toHaveBeenWarned()
   })
-
-  // test('daw', () => {
-  //   let apples = 0
-  //   const obj = {
-  //     getApples: () => 13,
-  //   }
-
-  //   const spy = vi.spyOn(obj, 'getApples').mockImplementation(() => apples)
-  //   apples = 1
-
-  //   expect(obj.getApples()).toBe(1)
-
-  //   expect(spy).toHaveBeenCalled()
-  //   expect(spy).toHaveReturnedWith(1)
-
-  //   // const getApples = vi.fn(() => 0)
-
-  //   // getApples()
-
-  //   // expect(getApples).toHaveBeenCalled()
-  //   // expect(getApples).toHaveReturnedWith(0)
-
-  //   // getApples.mockReturnValueOnce(5)
-
-  //   // const res = getApples()
-  //   // expect(res).toBe(5)
-  //   // expect(getApples).toHaveNthReturnedWith(2, 5)
-
-  // })
 })
