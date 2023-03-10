18db542d56021414fe8098edcc1f74ed6ef83ca7
dongwa <783137663@qq.com>
2023-03-10 23:24:27 +0800

    docs: remove sidebar-top of layout

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,12 +7,12 @@ import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
 import PreferenceSwitch from './components/PreferenceSwitch.vue'
 
 export default Object.assign({}, VPTheme, {
-  // ...DefaultTheme,
-  Layout: () => {
-    return h(VPTheme.Layout, null, {
-      'sidebar-top': () => h(PreferenceSwitch),
-    })
-  },
+  ...DefaultTheme,
+  // Layout: () => {
+  //   return h(VPTheme.Layout, null, {
+  //     'sidebar-top': () => h(PreferenceSwitch),
+  //   })
+  // },
   enhanceApp({ app }: { app: App }) {
   }
 })
