46abcc1533f48fc1f4d0736cffd06a9be35cd2c3
Dimples_YJ <2890841438@qq.com>
2023-03-20 22:03:34 +0800

    feat: add count down hook

---
+++ packages/vue/useCountDown/demo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useCountDown } from './index'
+
+const {days,hours, minutes, seconds } = useCountDown(1000 * 60 * 60 * 24)
+
+</script>
+
+<template>
+  <div>
+    {{ days }}天{{ hours }}时{{ minutes }}分{{ seconds }}秒
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ packages/vue/useCountDown/index.ts
@@ -0,0 +1,68 @@
+import { computed, ref } from 'vue-demi'
+
+interface CountDownOptions {
+  /**
+   * Start immediately
+   */
+  immediate?: boolean
+}
+
+/**
+ * Cont down
+ * @param time timestamp: endTime - startTime
+ * @param options
+ */
+export const useCountDown = (time: number, options?: CountDownOptions) => {
+  if (time <= 0) throw new Error('time must be greater than 0')
+
+  if (!options) {
+    options = { immediate: false }
+  }
+
+  const timeRef = ref(time)
+
+  const days = computed(() => Math.floor(timeRef.value / (1000 * 60 * 60 * 24)))
+  const hours = computed(() =>
+    Math.floor((timeRef.value % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+  )
+  const minutes = computed(() =>
+    Math.floor((timeRef.value % (1000 * 60 * 60)) / (1000 * 60)),
+  )
+  const seconds = computed(() =>
+    Math.floor((timeRef.value % (1000 * 60)) / 1000),
+  )
+  let timer: NodeJS.Timer | null
+
+  const countDown = () => {
+    if (timeRef.value <= 0) {
+      stop()
+      return
+    }
+    timeRef.value -= 1000
+  }
+
+  // pause
+  function stop() {
+    if (timer) {
+      clearInterval(timer)
+      timer = null
+    }
+  }
+
+  // start
+  function start() {
+    if (timer) return
+    timer = setInterval(countDown, 1000)
+  }
+
+  if (options?.immediate) start()
+
+  return {
+    days,
+    hours,
+    minutes,
+    seconds,
+    start,
+    stop,
+  }
+}
