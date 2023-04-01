6325d109c3e9d5653291a7d2eb6a1adccbc93a76
code-ManL <773890440@qq.com>
2023-04-01 15:19:05 +0800

    fix: publish version

--- node.env.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace NodeJS {
-    export interface ProcessEnv {
-      NODE_ENV:string
-    }
-  }
-}
-export {};
